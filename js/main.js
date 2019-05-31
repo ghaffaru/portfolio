ad1df2046868541eed7f31ed04473b22ff25e5b7
Ghaff <mudashiruagm@gmail.com>
2019-05-31 08:34:39 +0000

    Updates

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Select DOM elements
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set the initial State of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItems.forEach(item => item.classList.add("show"));
+
+    //show Menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //show Menu state
+    showMenu = false;
+  }
+}
